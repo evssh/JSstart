42835d96eb41e5924160d4ff1197e01bb2199f2f
kenzo <alarm88@mail.ru>
2019-08-03 18:30:21 +0300

    Tictactoe field with start button on js

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,11 @@
-//создает поле
+//контейнер
+
 function MakeField() {
-    //общий контейнер
-    let divcont = document.createElement('div');
-    divcont.className = 'container';
-    document.body.appendChild(divcont);
-    //ячейки
+  let divcont = document.createElement('div');
+  divcont.className = 'container';
+  divcont.id = 'container';
+  document.body.appendChild(divcont);
+  
     for (let i = 0; i < 49; i++) {
       let divcell = {};
       divcell[i] = document.createElement('div');
@@ -12,6 +13,22 @@ function MakeField() {
       divcell[i].className = 'cell';
       divcont.appendChild(divcell[i]);
     }
-}
-    
-MakeField();
+  }
+  
+  let field = 0;
+  
+  button.onclick = function(event) {
+    let   info = document.getElementById('info'),
+          button = document.getElementById('button');
+          divcont = document.getElementById('container');
+    if (field == 0) {
+      MakeField();
+      field++;
+      info.innerHTML = 'Ваш ход';
+      button.innerHTML = 'Начать заного';
+    }
+    if (field != 0) {
+      divcont.remove();
+      MakeField();
+    }
+  }  
